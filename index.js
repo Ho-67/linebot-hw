5bd97c5022dfa8fe363b84dc06e112e071f3dc76
Ho <ho67cnl@gmail.com>
2025-05-23 16:18:55 +0800

    index.js修改

--- index.js
+++ index.js
@@ -10,19 +10,16 @@ const bot = linebot({
 })
 
 bot.on('message', (event) => {
-  // 立即回應 200 OK
-  event.reply('收到訊息').then(() => {
-    if (event.message.type === 'location') {
-      commandAnimal(event)
-    } else if (event.message.type === 'text') {
-      const msg = event.message.text.toLowerCase()
-      if (msg === '認領養') {
-        commandQr(event) // 傳送選單
-      } else {
-        event.reply('請輸入「認領養」或傳送位置查詢動物 🐾')
-      }
+  if (event.message.type === 'location') {
+    commandAnimal(event)
+  } else if (event.message.type === 'text') {
+    const msg = event.message.text.toLowerCase()
+    if (msg === '認領養') {
+      commandQr(event) // 傳送選單
+    } else {
+      event.reply('請輸入「認領養」或傳送位置查詢動物 🐾')
     }
-  })
+  }
 })
 
 bot.on('postback', async (event) => {
