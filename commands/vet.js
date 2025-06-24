2e007ec2bb4bcc80c4aa287d48c5d00505a3805b
Ho <ho67cnl@gmail.com>
2025-06-24 22:52:38 +0800

    新增抓距離

--- commands/vet.js
+++ commands/vet.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { distance } from '../utils/distance.js'
 import template from '../templates/vet.js'
+import { geocodeAddress } from '../utils/geocode.js'
 
 export default async (event) => {
   try {
@@ -8,25 +9,29 @@ export default async (event) => {
       'https://data.moa.gov.tw/Service/OpenData/DataFileService.aspx?UnitId=078&IsTransData=1',
     )
 
-    const bubbles = data
-      // 加上距離單位，紀錄每個東西離使用者的位置多遠
-      .map((value) => {
-        value.distance = distance(
-          value.Latitude,
-          value.Longitude,
-          event.message.latitude,
-          event.message.longitude,
-          'K',
-        )
-        return value
-      })
-      // 依照距離欄位的值排序(小到大)
-      .sort((a, b) => {
-        return a.distance - b.distance
-      })
-      // 取出前三筆
+    const bubbles = []
+
+    for (const value of data) {
+      let lat = value.Latitude
+      let lon = value.Longitude
+
+      // 呼叫 geocoding API 取得經緯度
+      if (!lat || !lon) {
+        const geo = await geocodeAddress(value.機構地址)
+        if (!geo) continue // 取不到經緯度就跳過
+        lat = geo.lat
+        lon = geo.lon
+      }
+
+      // 加上距離欄位
+      value.distance = distance(lat, lon, event.message.latitude, event.message.longitude, 'K')
+
+      bubbles.push(value)
+    }
+
+    const sorted = bubbles
+      .sort((a, b) => a.distance - b.distance)
       .slice(0, 3)
-      // 套用 flex 模板
       .map((value) => {
         const bubble = template()
 
@@ -34,8 +39,10 @@ export default async (event) => {
         bubble.body.contents[1].text = value.機構地址 || '無地址資訊'
         bubble.body.contents[2].text = `距離：約 ${value.distance.toFixed(1)} 公里`
 
-        bubble.footer.contents[0].action.uri = `tel:${value.機構電話?.replace(/[^\d]/g, '') || ''}` // 把電話號碼中所有非數字字元移除，只保留純數字
-        bubble.footer.contents[0].action.label = value.機構電話 || '撥打電話'
+        bubble.footer.contents[0].action.uri = `tel:${value.機構電話?.replace(/[^\d]/g, '') || ''}`
+        bubble.footer.contents[0].action.label = value.機構電話
+          ? `撥打：${value.機構電話}`
+          : '撥打電話'
 
         bubble.footer.contents[1].action.uri = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(value.機構地址)}`
         bubble.footer.contents[1].action.label = 'Google地圖'
@@ -43,13 +50,12 @@ export default async (event) => {
         return bubble
       })
 
-    // 將 bubble 回傳給使用者
     await event.reply({
       type: 'flex',
       altText: '附近的動物醫院',
       contents: {
         type: 'carousel',
-        contents: bubbles,
+        contents: sorted,
       },
     })
   } catch (error) {
